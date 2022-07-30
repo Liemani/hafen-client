9587cd51bf717171ff86296b9ad87362d2ca6a05
Liemani <parkjh2768@naver.com>
2022-07-31 00:35:57 +0900

    update lmi

--- src/lmi/MainThread.java
+++ src/lmi/MainThread.java
@@ -6,11 +6,20 @@ import java.util.Set;
 import java.util.concurrent.Callable;
 
 public class MainThread implements Runnable {
+    // fields
+    haven.MainFrame mainFrame_;
+    haven.UIPanel uiPanel_;
+
+    public MainThread(haven.MainFrame mainFrame, haven.UIPanel uiPanel) {
+        mainFrame_ = mainFrame;
+        uiPanel_ = uiPanel;
+    }
+
     // Runnable requirment
     public void run() {
         System.out.println("[top of run()]");
 
-        initThread();
+        this.init();
 
         while (true) {
             MainThread.printCommandStringList(System.out);
@@ -32,12 +41,7 @@ public class MainThread implements Runnable {
         }
     }
 
-    // private methods
-    private static void initThread() {
-        Scanner.initScanner(System.in);
-    }
-
-    // private methods
+    // private class methods
     private static void printCommandStringList(PrintStream outputStream) {
         outputStream.println("command list:");
         Set<String> commandStringSet = lmi.Command.getCommandStringSet();
@@ -46,8 +50,16 @@ public class MainThread implements Runnable {
         });
     }
 
+    // private instance methods
+    private void init() {
+        lmi.Scanner.init(System.in);
+        lmi.Command.init(this);
+    }
+
     // main()
     public static void main(String args[]) {
-        new Thread(new lmi.MainThread()).start();
+        lmi.MainThread mainThread = new lmi.MainThread(new haven.MainFrame(null), new haven.JOGLPanel(null));
+        new Thread(mainThread).start();
+//      new Thread(new lmi.MainThread(this, this.p)).start();
     }
 }
