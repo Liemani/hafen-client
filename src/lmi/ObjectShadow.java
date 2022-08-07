4324efd058ff39cf659e25c4e15c8535ba1d9519
Liemani <parkjh2768@naver.com>
2022-08-07 10:06:50 +0900

    creat lmi.Debug.object\(Act)ResultOfInvoikedMethod()

--- src/lmi/ObjectShadow.java
+++ src/lmi/ObjectShadow.java
@@ -10,6 +10,7 @@ public class ObjectShadow {
     public static haven.UI ui_;
     public static haven.RootWidget rootWidget_;
     public static haven.GameUI gameUI_;
+    public static haven.MapView mapView_;
     public static haven.Session session_;
 
     // init()
@@ -23,8 +24,24 @@ public class ObjectShadow {
     public static void setUI(haven.UI ui) {
         ui_ = ui;
         setRootWidget(ui.root);
-        setGameUI((haven.GameUI)rootWidget_.child);
         setSession(ui_.sess);
+    }
+
+    public static void setRootWidget(haven.RootWidget rootWidget) {
+        rootWidget_ = rootWidget;
+    }
+
+    public static void setGameUI(haven.GameUI gameUI) {
+        gameUI_ = gameUI;
+    }
+
+    public static void setMapView(haven.MapView mapView) {
+        mapView_ = mapView;
+        Player.init(mapView_.player());
+    }
+
+    public static void setSession(haven.Session session) {
+        session_ = session;
     }
 
     // package methods
@@ -38,18 +55,5 @@ public class ObjectShadow {
 
     static void closeSession() {
         ui_.sess.close();
-    }
-
-    // private methods
-    private static void setRootWidget(haven.RootWidget rootWidget) {
-        rootWidget_ = rootWidget;
-    }
-
-    private static void setGameUI(haven.GameUI gameUI) {
-        gameUI_ = gameUI;
-    }
-
-    private static void setSession(haven.Session session) {
-        session_ = session;
     }
 }
