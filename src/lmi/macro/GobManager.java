22d91e21de9cd1f0fc6d91c72b12bd3247143fa8
Liemani <parkjh2768@naver.com>
2022-08-09 23:50:26 +0900

    update lmi.macro.GobManager

--- src/lmi/macro/GobManager.java
+++ src/lmi/macro/GobManager.java
@@ -9,4 +9,24 @@ public class GobManager {
         //  System.out.println(it.next());
     //  }
     static java.util.Iterator<haven.Gob> iterator() { return objectCache_.iterator(); }
+
+    static haven.Coord2d location(haven.Gob gob) {
+        return gob.rc;
+    }
+
+    static boolean isStop(haven.Gob gob) {
+        return velocity(gob) == 0.0;
+    }
+
+    static boolean isMoving(haven.Gob gob) {
+        return velocity(gob) != 0.0;
+    }
+
+    static haven.Resource resource(haven.Gob gob) {
+        return gob.getres();
+    }
+
+    static double velocity(haven.Gob gob) {
+        return gob.getv();
+    }
 }
