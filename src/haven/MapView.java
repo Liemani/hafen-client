6b272fdfc2b21b967ec331bc0b3431dc8b86d844
Liemani <parkjh2768@naver.com>
2022-08-09 22:16:30 +0900

    create lmi.macro.GobManager

--- src/haven/MapView.java
+++ src/haven/MapView.java
@@ -1938,6 +1938,7 @@ public class MapView extends PView implements DTarget, Console.Directory {
 	protected void hit(Coord pc, Coord2d mc, ClickData inf) {
         if (inf != null && inf.ci instanceof Gob.GobClick)
             lmi.macro.Util.storeLastClickedGob(((Gob.GobClick)inf.ci).gob);
+            //  lmi.macro.Util.storeLastClickedGobByClickData(inf);
 	    Object[] args = {pc, mc.floor(posres), clickb, ui.modflags()};
 	    if(inf != null)
 		args = Utils.extend(args, inf.clickargs());
