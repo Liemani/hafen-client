1af2ed2a84e4e844aa003d6d1744ab41cc3e279a
Liemani <parkjh2768@naver.com>
2022-08-09 17:17:43 +0900

    create lmi.macro.AutoConnect

--- src/lmi/macro/Util.java
+++ src/lmi/macro/Util.java
@@ -6,14 +6,7 @@ public class Util {
     public static void storeLastClickedGob(haven.Gob gob) { gob_ = gob; }
     public static void printLastClickedGob() { lmi.Debug.debugDescribeField(gob_); }
 
-//      public static void autoConnect() {
-//          // TODO move awt functions to lmi.macro package
-//          click login
-//          if (test exist)
-//              selectCharacter("test");
-//      }
-
     public static void selectCharacter(String name) {
-        lmi.ObjectShadow.charList_.wdgmsg(lmi.Constant.Command.SELECT_CHARACTER, name);
+        lmi.ObjectShadow.characterList_.wdgmsg(lmi.Constant.Command.SELECT_CHARACTER, name);
     }
 }
